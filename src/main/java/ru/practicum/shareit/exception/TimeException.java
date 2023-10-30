25bcd842319ac100c50cd090be8f1a5a8674d55d
allen11137 <122568848+allen11137@users.noreply.github.com>
2023-10-31 00:29:38 +0300

    Update 30.10 After review

--- src/main/java/ru/practicum/shareit/exception/TimeException.java
+++ src/main/java/ru/practicum/shareit/exception/TimeException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class TimeException extends RuntimeException{
+public class TimeException extends RuntimeException {
     public TimeException(String message) {
         super(message);
     }
